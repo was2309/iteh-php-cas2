1a4f48edd31e6dfa5752bbc828970929d6a07589
Tamara Naumovic <tamaranaumovic@gmail.com>
2021-10-13 15:56:52 +0200

    logovanje na index.php

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+
+require "dbBroker.php";
+require "model/user.php";
+
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+    $conn = new mysqli();
+
+    $korisnik = new User(1, $uname, $upass);
+    // $odg = $korisnik->logInUser($uname, $upass, $conn);
+    $odg = User::logInUser($korisnik, $conn); //pristup statickim funkcijama preko klase
+
+    if($odg->num_rows==1){
+        echo  `
+        <script>
+        console.log( "Uspešno ste se prijavili");
+        </script>
+        `;
+        $_SESSION['user_id'] = $korisnik->id;
+        header('Location: home.php');
+        exit();
+    }else{
+        echo `
+        <script>
+        console.log( "Niste se prijavili!");
+        </script>
+        `;
+    }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
