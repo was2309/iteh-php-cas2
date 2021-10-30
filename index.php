451d31c4b373b441652ded4f09105bcc8bab33c7
Vasilije Mijuskovic <vasilijem92@gmail.com>
2021-10-30 16:15:39 +0200

    logovanje na index.php

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+
+require "dbBroker.php";
+require "model/user.php";
+
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+    $conn = new mysqli();
+
+    $korisnik = new User(null, $uname, $upass);
+
+    // $odg = $korisnik->logInUser($uname, $upass, $conn);
+    $odg = User::logInUser($korisnik,$conn);
+
+    if($odg->num_rows==1){
+        echo `<script>
+        console.log("Uspesno ste se prijavili.");
+        </script>`;
+
+        $_SESSION['user_id'] = $korisnik->id;
+        header('Location: home.php');
+        exit();
+    }else{
+        echo `<script>
+        console.log("Niste se prijavili.");
+        </script>`;
+    }
+
+
+}
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
