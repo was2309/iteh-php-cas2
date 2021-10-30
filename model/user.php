f1756a683a788156799e27b11ebe0e3d5d986d58
Vasilije Mijuskovic <vasilijem92@gmail.com>
2021-10-30 16:14:38 +0200

    kreirani user i prijava

---
+++ model/user.php
@@ -0,0 +1,28 @@
+<?php
+
+class User{
+    public $id;
+    public $username;
+    public $password;
+
+    public function __construct($id=null, $username=null, $password=null)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public static function logInUser($usr, mysqli $conn)
+    {
+        $query = "SELECT * FROM user WHERE username='$usr->username' and password='$usr->password'";
+        //Konekcija sa bazom.
+        return $conn->query($query);
+    }
+
+}
+
+
+
+
+
+?>
